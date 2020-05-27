68dbcae67c9c012137a1a2b467081f0efedad65d
wrscomputers <wrscomputers@gmail.com>
2020-05-26 20:50:06 -0400

    Working on Persistant Storage

--- DontForget/Controller/Model Controller/NotesList.swift
+++ DontForget/Controller/Model Controller/NotesList.swift
@@ -12,5 +12,44 @@ class NotesList {
     
     var notesCreated: [Notes] = []
     
+    init() {
+        loadFromPersistentStore()
+    }
+    
+    func createItem() {
+        saveToPersistanceStore()
+    }
+}
+
+//MARK: - Persistence
+var persistentFileURL: URL? {
+    let fileManager = FileManager.default
+    let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+    let notesURL = documentsDir?.appendingPathComponent("notes.plist")
+    return notesURL
+}
+
+func saveToPersistanceStore() {
+    do {
+        guard let persistentFileURL = persistentFileURL else {return}
+        let encoder = PropertyListEncoder()
+        let notesPlist = try encoder.encode(notesCreated)
+        try notesPlist.write(to: persistentFileURL)
+    } catch {
+        print("Error saving items: \(error)")
+    }
+}
+
+func loadFromPersistentStore() {
+    do {
+        guard let persistentFileURL = persistentFileURL else { return }
+        let notesPlist = try Data(contentsOf: persistentFileURL)
+        let decoder = PropertyListDecoder()
+        let decodeditems = try decoder.decode([Notes].self, from: notesPlist)
+        self.notes = decodeditems
+    } catch {
+        print("Error loading items from plist: \(error)")
+    }
+}
     
 }
